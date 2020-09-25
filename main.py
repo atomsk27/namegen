365640892bcdec7df8a80a7a1a6bcd895af8a242
Bryan Manzano <bryanmmr@gmail.com>
2020-09-25 10:43:44 -0500

    gen pass

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import names
 import namegenerator as namegen
+import secrets
+import string
 
 user = names.get_full_name()
 prof = namegen.gen()
+alphabet = string.ascii_letters + string.digits
+password = ''.join(secrets.choice(alphabet) for i in range(16))
 
-
-print('Nombres: ', user, '\nUsername: ', prof)
+print('Nombres: ', user, '\nUsername: ', prof, '\nPassword: ', password)
